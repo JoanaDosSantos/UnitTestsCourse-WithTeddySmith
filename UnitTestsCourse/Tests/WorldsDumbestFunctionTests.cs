ad91addd4aed4f7da1a0af5a3a1527c097f3d0ed
Joana Santos <Joana_Santos004@outlook.com>
2023-12-14 11:54:17 -0300

    Adicionar arquivos de projeto.

---
+++ UnitTestsCourse/Tests/WorldsDumbestFunctionTests.cs
@@ -0,0 +1,35 @@
+﻿namespace UnitTestsCourse.Tests
+{
+    public static class WorldsDumbestFunctionTests
+    {
+        //Pay attentions to naming conventions - ClassName_Method_ExpectedResult
+        //Tests don't return anything, so they're always void
+        public static void WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnsString()
+        {
+            try
+            {
+                //Arrange - Go get your variables, classes, functions, whatever you need
+                int num = 0;
+                WorldsDumbestFunction worldsDumbest = new WorldsDumbestFunction();
+
+                //Act - Execute this function
+                string result = worldsDumbest.ReturnsPikachuIfZero(num);
+
+                //Assert - Whatever ever is returned is it whats you want 
+                if(result == "PIKACHU!") 
+                {
+                    Console.WriteLine("PASSED: WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnsString");
+                }
+                else
+                {
+                    Console.WriteLine("FAILED: WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnsString");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        
+    }
+}
---
+++ UnitTestsCourse/WorldsDumbestFunction.cs
@@ -0,0 +1,17 @@
+﻿namespace UnitTestsCourse
+{
+    public class WorldsDumbestFunction
+    {
+        public string ReturnsPikachuIfZero(int num)
+        {
+            if (num == 0) 
+            { 
+                return "PIKACHU!"; 
+            }
+            else
+            {
+                return "Squirtle";
+            }
+        }
+    }
+}
