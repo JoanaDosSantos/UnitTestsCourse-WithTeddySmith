3c413e166e59a9d03253f1eb5e8e2f591c94d9b2
Joana Santos <Joana_Santos004@outlook.com>
2023-12-14 16:26:54 -0300

    Code final version of second and third class on Unit Testing in C# 2022 with Teddy Smith on Youtube.

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,112 @@
+﻿using FluentAssertions;
+using FluentAssertions.Extensions;
+using NetworkUtility.Ping;
+using System.Net.NetworkInformation;
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        # region Constructor
+        private readonly NetworkService _pingService;
+        public NetworkServiceTests() 
+        {
+            //SUT
+            _pingService = new NetworkService();
+        }
+        #endregion
+
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            //Arrange - variables, classes, mocks
+            //Act
+            var result = _pingService.SendPing();
+
+            //Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent");
+            result.Should().Contain("Success", Exactly.Once());
+            //Exactly make sure the string appears at least 1 time
+        }
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, 2, 4)]
+        public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
+        {
+            //Arrange
+            //Act
+            var result = _pingService.PingTimeout(a, b);
+
+            //Assert
+            result.Should().Be(expected);
+            result.Should().BeGreaterThanOrEqualTo(2);
+            result.Should().NotBeInRange(-1000, 0);
+
+        }
+
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnDate()
+        {
+            //Arrange - variables, classes, mocks
+            //Act
+            var result = _pingService.LastPingDate();
+
+            //Assert
+            result.Should().BeAfter(1.January(2010));
+            result.Should().BeBefore(1.January(2030));
+        }
+
+        [Fact]
+        public void NetworkService_GetPingOptions_ReturnObject()
+        {
+            //Arrange - variables, classes, mocks
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            //Act
+            var result = _pingService.GetPingOptions();
+
+            //Assert WARNING: "Be" careful 
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+        }
+
+        [Fact]
+        public void NetworkService_MostRecentPings_ReturnObject()
+        {
+            //Arrange - variables, classes, mocks
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            //Act
+            var result = _pingService.MostRecentPings();
+
+            //Assert WARNING: "Be" careful 
+            result.Should().BeOfType<PingOptions[]>();
+            result.Should().ContainEquivalentOf(expected);
+            result.Should().Contain(x => x.DontFragment == true);
+        }
+    }
+}
+
+/*
+ * The [Fact] attribute is used by xUnit.net to identify a ‘normal’ 
+ * unit test: a test method that does not take any method arguments. 
+ * 
+ * The [Theory] attribute, on the other hand, expects one or more instances
+ * of the DataAttribute to provide the values for the arguments of the 
+ * parameterized test method.
+ * 
+ * The [InlineData] attribute is one of several attributes derived from 
+ * DataAttribute that you can use with your Theory. It allows you to 
+ * provide fixed values for the test method arguments
+ */
